a693d1eef3fd652810b0633db9c920c39457edaf
Emmanuel <preiraclement79@gmail.com>
2024-07-03 04:07:12 +0100

    first commit

---
+++ First/First/src/main/java/com/example/First/Controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.First.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/")
+    public String index() {
+        return "Greeting from Spring Boot!";
+    }
+
+
+}
